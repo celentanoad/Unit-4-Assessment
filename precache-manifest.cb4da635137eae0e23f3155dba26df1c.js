1c2fb81188a9fbd9f4466444bcfe0bcaa37d1f22
Alanna Celentano <alannacelentano@alannas-mbp.attlocal.net>
2020-04-14 10:46:42 -0500

    Updates

---
+++ precache-manifest.cb4da635137eae0e23f3155dba26df1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18dfccfee136eb05290845cf722edb99",
+    "url": "/Unit-4-Assessment/index.html"
+  },
+  {
+    "revision": "4d980dea5101b1fed076",
+    "url": "/Unit-4-Assessment/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "d0ea1ea29cf86d05ee7b",
+    "url": "/Unit-4-Assessment/static/js/2.143933b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Unit-4-Assessment/static/js/2.143933b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d980dea5101b1fed076",
+    "url": "/Unit-4-Assessment/static/js/main.0b22c794.chunk.js"
+  },
+  {
+    "revision": "a18fac5a9b0c3a6caf46",
+    "url": "/Unit-4-Assessment/static/js/runtime-main.e9132f46.js"
+  }
+]);
